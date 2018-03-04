fac61b0f5d0e5bc5139c98e3a61cf9349ba5a248
Md Sazzad Hissain Khan <hissain.khan@gmail.com>
2018-03-04 10:50:41 +0600

    Create qsort.cpp

---
+++ qsort.cpp
@@ -0,0 +1,40 @@
+void swap(int *a, int *b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int pivote(int data[], int s, int e){
+    
+    int pIndex = s + (e - s) / 2;
+    int pValue = data[pIndex];
+    swap(data + pIndex, data + e);
+    int c = s;
+    for(register int i = s; i < e; i++){
+        if(pValue < data[i]){
+            swap(data + i, data + c);
+            c++;
+        }
+    }
+    swap(data + c, data + e);
+    return c;
+}
+
+
+void qsort(int data[], int s, int e){
+    if(s < e){
+        int p = pivote(data, s, e);
+        qsort(data, s, p - 1);
+        qsort(data, p + 1, e);
+    }
+}
+
+
+int main(){
+
+    int data[5] = {2, 5, 10, 8, 3};
+    
+    qsort(data, 0, 4);
+    
+    return 0;
+}
